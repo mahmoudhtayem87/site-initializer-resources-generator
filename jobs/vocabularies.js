3663b92de917a9f3ee589a5229faab3889710fcd
Jan Verweij <jan.verweij@liferay.com>
2022-03-22 17:21:42 +0100

    basic json + directory creation

--- jobs/vocabularies.js
+++ jobs/vocabularies.js
@@ -5,7 +5,7 @@ var dir = './output/resources/site-initializer/taxonomy-vocabularies/group';
 //http://localhost:8080/o/api?endpoint=http://localhost:8080/o/headless-admin-taxonomy/v1.0/openapi.json
 
 
-async function checkFolder() {
+async function createFolder(dir) {
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, { recursive: true });
     }
@@ -20,11 +20,18 @@ async function createFile(filedata, filename) {
 }
 async function createVocabulary(vocabulary) {
     console.log("voc: " + vocabulary.name);
-    await createFile("{}", vocabulary.name + ".json");
+    let vocJson = {
+        "description": vocabulary.description,
+        "externalReferenceCode": vocabulary.name.toUpperCase().replace(/\s/g, ''),
+        "name": vocabulary.name,
+        "viewableBy": "Anyone"
+    };
+    await createFile(JSON.stringify(vocJson), vocabulary.name.toLowerCase() + ".json");
+    await createFolder(dir + "/" + vocabulary.name.toLowerCase());
 }
 async function start() {
     console.log('Creating vocabularies');
-    await checkFolder();
+    await createFolder(dir);
     var data = await applications.getVocabularies();
     console.log(data);
     //loop items and create json file and folder
